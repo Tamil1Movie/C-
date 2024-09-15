07337412a894a9f816c740772b2a6cc688494a64
Tamil1Movie <167101844+Tamil1Movie@users.noreply.github.com>
2024-09-15 13:05:12 +0530

    Update singlely_link.c

--- singly_link.c
+++ singly_link.c
@@ -1,13 +1,29 @@
-#include<stdio.h>
-struct node
-{
-  int data 
-  struct node *next;
-}*temp;
-int main()
-{
-  temp=(struct node*)malloc(sizeof(struct node));
-  temp->data=data;
-  temp->next=Null
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    struct Node* head = NULL;
+    struct Node* node1 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* node2 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* node3 = (struct Node*)malloc(sizeof(struct Node));
+    node1->data = 10;
+    node1->next = node2;
+    node2->data = 20;
+    node2->next = node3;
+    node3->data = 30;
+    node3->next = NULL;
+    head = node1;
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+
+    return 0;
 }
