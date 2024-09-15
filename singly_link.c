e1d2aacd8fd031e58db7d524f3dfc04afd41b9e4
Tamil1Movie <167101844+Tamil1Movie@users.noreply.github.com>
2024-09-15 12:44:58 +0530

    Create singly_link.c

---
+++ singly_link.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+struct node
+{
+  int data 
+  struct node *next;
+}*temp;
+int main()
+{
+  temp=(struct node*)malloc(sizeof(struct node));
+  temp->data=data;
+  temp->next=Null
+  return 0;
+}
