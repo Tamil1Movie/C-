86b8f18b9760eb6fba27083a828296d7962110e2
Tamil1Movie <167101844+Tamil1Movie@users.noreply.github.com>
2024-09-15 13:14:09 +0530

    Create singlely_insert_begining.c

---
+++ singlely_insert_begining.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+int main() 
+{
+    struct Node* head = NULL;
+    struct Node* node1 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* node2 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* node3 = (struct Node*)malloc(sizeof(struct Node));
+    node1->data = 10;
+    node1->next = node2;
+    node2->data = 20;
+    node2->next = node3;
+    node3->data = 30;
+    node3->next = NULL;
+    head = node1;
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = 50;
+    newNode->next = head;
+    head = newNode;
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+
+    return 0;
+}
